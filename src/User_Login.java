86e97fee0fe08896b164ed198ceddacf71207ff4
corerman <admin@icorer.com>
2015-06-14 13:19:41 +0800

    user login event listen

--- src/User_Login.java
+++ src/User_Login.java
@@ -8,12 +8,21 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
 import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 
 public class User_Login extends JFrame {
 
 	private JPanel contentPane;
-
+	private JTextField textField;
+	private JTextField textField_1;
+	
 	/**
 	 * Launch the application.
 	 * @throws UnsupportedLookAndFeelException 
@@ -45,15 +54,64 @@ public class User_Login extends JFrame {
 	 */
 	public User_Login() {
 		setResizable(false);
-		setOpacity(0.8f);
+		setOpacity(0.9f);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 317, 224);
+		setBounds(100, 100, 314, 178);
 		contentPane = new JPanel();
-		contentPane.setBackground(SystemColor.inactiveCaptionText);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
 
+		contentPane.setBackground(new Color(85, 107, 47));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				if(textField.getText().compareTo("用户名：")==0)
+					textField.setText("");
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(textField.getText().compareTo("")==0)
+					textField.setText("用户名：");
+			}
+		});
+
+		
+		textField.setToolTipText("");
+		textField.setBounds(60, 23, 200, 30);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(textField_1.getText().compareTo("密码：")==0)
+					textField_1.setText("");
+				
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(textField_1.getText().compareTo("")==0)
+					textField_1.setText("密码：");		
+			}
+		});
+		//textField_1.setToolTipText("");
+		textField_1.setText("密码：");
+		textField.setText("用户名：");
+		textField_1.setBounds(60, 66, 200, 30);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("\u767B\u5F55");
+		btnNewButton.setBounds(70, 106, 74, 34);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("\u6CE8\u518C");
+		btnNewButton_1.setBounds(167, 106, 74, 34);
+		contentPane.add(btnNewButton_1);
+	}
 }
