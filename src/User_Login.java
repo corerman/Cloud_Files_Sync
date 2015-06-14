76d5c31f80fcd38132bd67eca2e86776953653b4
corerman <admin@icorer.com>
2015-06-14 14:25:29 +0800

    passwordFilde Change

--- src/User_Login.java
+++ src/User_Login.java
@@ -15,14 +15,21 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.InputMethodEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPasswordField;
 
 
 public class User_Login extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField username;
-	private JTextField password;
-	
+	private String password_content;
+	private JPasswordField password;
 	/**
 	 * Launch the application.
 	 * @throws UnsupportedLookAndFeelException 
@@ -84,28 +91,8 @@ public class User_Login extends JFrame {
 		username.setBounds(60, 23, 200, 30);
 		contentPane.add(username);
 		username.setColumns(10);
-		
-		password = new JTextField();
-		password.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				if(password.getText().compareTo("密码：")==0)
-					password.setText("");
-				
-			}
-			@Override
-			public void focusLost(FocusEvent e) {
-				if(password.getText().compareTo("")==0)
-					password.setText("密码：");		
-			}
-		});
-		//textField_1.setToolTipText("");
-		password.setText("密码：");
 		username.setText("用户名：");
-		password.setBounds(60, 66, 200, 30);
-		contentPane.add(password);
-		password.setColumns(10);
-		
+	
 		JButton btnNewButton = new JButton("\u767B\u5F55");
 		btnNewButton.setBounds(70, 106, 74, 34);
 		contentPane.add(btnNewButton);
@@ -113,5 +100,27 @@ public class User_Login extends JFrame {
 		JButton btnNewButton_1 = new JButton("\u6CE8\u518C");
 		btnNewButton_1.setBounds(167, 106, 74, 34);
 		contentPane.add(btnNewButton_1);
+		
+		password = new JPasswordField();
+		password.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				char[] temp=password.getPassword(); //获取密码初始指针
+				String pass_content=new String(temp);
+				if(pass_content.compareTo("..........")==0)
+					password.setText("");
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				char[] temp=password.getPassword(); //获取密码初始指针
+				String pass_content=new String(temp);
+				if(pass_content.compareTo("")==0)
+					password.setText("..........");
+			}
+		});
+		password.setToolTipText("");
+		password.setText("..........");
+		password.setBounds(60, 63, 200, 30);
+		contentPane.add(password);
 	}
 }
